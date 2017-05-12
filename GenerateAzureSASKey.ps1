57a3bb6a06a4e4cfcdaa6ddb1fb44c1406f4dc0c
Marcus Felling <MarcusFelling@users.noreply.github.com>
2017-05-12 16:55:43 -0500

    Add files via upload

---
+++ GenerateAzureSASKey.ps1
@@ -0,0 +1,9 @@
+﻿
+
+$storageAccountName = ""
+$storageAccountKey = ""
+$container = ""
+
+$context = New-AzureStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageAccountKey
+$sas = New-AzureStorageContainerSASToken -Name $container -Permission rl -Context $context -ExpiryTime ([DateTime]::UtcNow.AddDays(7)) 
+Write-Host "$($context.BlobEndPoint)$($container)$($sas)"
