fb3a7bf11a097b01dd2ec3bd1bb58073173b863a
Marcus Felling <MarcusFelling@users.noreply.github.com>
2019-01-13 10:51:06 -0600

    Improve output report

--- AzureDevOps_Get-ReleasesToPromote.ps1
+++ AzureDevOps_Get-ReleasesToPromote.ps1
@@ -4,7 +4,7 @@
    but not yet promoted to Pre-Prod or Prod environments.
 
 .Outputs
-   "C:\Temp\LatestQADeployments.csv" 
+   "C:\Temp\LatestReleases.html"
 #>
 
 [CmdletBinding()]
@@ -13,9 +13,8 @@ Param
     [Parameter(Mandatory=$true)]
     $PAT, # Personal Access Token
     [Parameter(Mandatory=$false)]
-    $TFSBaseURL
+    $TFSBaseURL 
 )
-
 # Base64-encodes the Personal Access Token (PAT) appropriately
 # This is required to pass PAT through HTTP header
 $script:User = "" # Not needed when using PAT, can be set to anything
@@ -27,6 +26,14 @@ $script:Base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetByt
 # Invoke the REST call and capture the response
 $GetDefinitionsUriResponse = Invoke-RestMethod -Uri $GetDefinitionsUri -Method Get -Headers @{Authorization=("Basic {0}" -f $Base64AuthInfo)}
 $DefinitionIDs = $GetDefinitionsUriResponse.value.id
+
+# Create custom object to store output in, that can be used to build HTML report.
+$objTemplateObject = New-Object psobject
+$objTemplateObject | Add-Member -MemberType NoteProperty -Name DefinitionName -Value $null
+$objTemplateObject | Add-Member -MemberType NoteProperty -Name Link -Value $null
+
+# Create empty array which will become the output object
+$objResult = @()
 
 # Use definition ID's to loop and get latest deployments of each definition
 ForEach($DefinitionID in $DefinitionIDs){
@@ -61,12 +68,34 @@ ForEach($DefinitionID in $DefinitionIDs){
     $NoDeploymentReleaseDefinitionName = ""
     $NoDeploymentReleaseDefinitionName = $NoDeployment.releaseDefinition.name
     
-    If($NoDeployment){
-		$NoDeploymentReleaseDefinitionName | Select-Object -first 1
-		$LatestDeployment.release.webAccessUri
+    If($NoDeployment){            
+        # Write output to Azure Pipeline log
+        $NoDeploymentReleaseDefinitionName | Select-Object -first 1
+        $LatestDeployment.release.webAccessUri
 
-		# Output to CSV file that is sent via email in release definition
-		$NoDeploymentReleaseDefinitionName | Select-Object -first 1 | Out-File "C:\Temp\LatestQADeployments.csv" -Append
-		$URLs += $LatestDeployment.release.webAccessUri | Out-File "C:\Temp\LatestQADeployments.csv" -Append
+        # Create an instance of new object to prepare it with data and later add it to the result array for report          
+        $objTemp = $objTemplateObject | Select-Object *
+
+        # Populate the custom object properties
+        $objTemp.DefinitionName = $NoDeploymentReleaseDefinitionName | Select-Object -first 1
+        $objTemp.Link = $LatestDeployment.release.webAccessUri 
+
+        # Add temp object to output array and get ready to loop back around
+        $objResult += $objTemp
     }
 }
+
+# Set CSS properties for HTML report 
+$Header = @"
+<style>
+TABLE {border-width: 1px; border-style: solid; border-color: black; border-collapse: collapse;}
+TH {border-width: 1px; padding: 3px; border-style: solid; border-color: black; background-color: #6495ED;}
+TD {border-width: 1px; padding: 3px; border-style: solid; border-color: black;}
+</style>
+"@
+
+# Output to HTML file that is sent via email in release definition
+$objResult = $objResult | 
+ConvertTo-Html @{Label="DefinitionName";Expression={$_.DefinitionName}},@{Label="Link";Expression={ "<a href='$($_.Link)'>$($_.Link)</a>" }} -Head $Header
+Add-Type -AssemblyName System.Web
+[System.Web.HttpUtility]::HtmlDecode($objResult) | Out-File "C:\Temp\LatestReleases.html"
